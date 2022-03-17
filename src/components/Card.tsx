a31a6981a735c338de038a7a8207ec57d9ac9e52
Dusan <dmomcilo@hotmail.com>
2022-03-17 12:54:07 -0400

    css styling work

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,27 +11,40 @@ const Card = ({ movie }: Props) => {
   const { addMovie, removeMovie, isFav } = useContext(WatchlistContext);
   console.log(movie);
   return (
-    <li className="Card">
-      <Link
-        to={`/movies/${encodeURIComponent(movie.id)}/details`}
-        style={{ textDecoration: "none", color: "black" }}
-      >
-        <p className="movieTitle">{movie.original_title}</p>
-        <img
-          className="moviePoster"
-          src={`https://www.themoviedb.org/t/p/w220_and_h330_face${movie.poster_path}`}
-          alt={movie.original_title}
-        />
-      </Link>
-      {isFav(movie.id) ? (
-        <i
-          className="fa-solid fa-star"
-          onClick={() => removeMovie(movie.id)}
-        ></i>
-      ) : (
-        <i className="fa-regular fa-star" onClick={() => addMovie(movie)}></i>
+    <>
+      {movie.original_title && (
+        <li className="Card">
+          <Link
+            to={`/movies/${encodeURIComponent(movie.id)}/details`}
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            {movie.original_title ? (
+              <p className="movieTitle">{movie.original_title}</p>
+            ) : (
+              <p>Not Found</p>
+            )}
+
+            <img
+              className="moviePoster"
+              src={`https://www.themoviedb.org/t/p/w220_and_h330_face${movie.poster_path}`}
+              alt={movie.original_title}
+            />
+          </Link>
+
+          {isFav(movie.id) ? (
+            <i
+              className="fa-solid fa-star"
+              onClick={() => removeMovie(movie.id)}
+            ></i>
+          ) : (
+            <i
+              className="fa-regular fa-star"
+              onClick={() => addMovie(movie)}
+            ></i>
+          )}
+        </li>
       )}
-    </li>
+    </>
   );
 };
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,22 @@
 import "./Header.css";
 import SearchBar from "./SearchBar";
 import Filters from "./Filters";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const Header = () => {
+  // const id: string | undefined = useParams().id;
+  // console.log(useParams());
+
   return (
     <header className="Header">
       <h1>What's Good?</h1>
-      <div>
-        <SearchBar />
-        <Filters />
+      <div className="headerDiv">
+        {/* {!id && ( */}
+        <div className="containerSearchandFilter">
+          <SearchBar />
+          <Filters />
+        </div>
+        {/* // )} */}
         <Link to="/movies/watchlist" style={{ textDecoration: "none" }}>
           <p>Watchlist</p>
         </Link>
